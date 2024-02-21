8fef48c123787e8361cab6517f2a324540f0d394
VishnurajKarockal <vishnurajkarockal@gmail.com>
2024-02-21 22:49:40 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        Facebook
+      </div>
+      
+    </>
+  )
+}
+
+export default App
