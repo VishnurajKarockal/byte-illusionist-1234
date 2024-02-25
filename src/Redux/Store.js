d8deab5d37264c5410d72f8dde33b7e15fbd54d1
Sonali Burman <sonali.b.020200@gmail.com>
2024-02-25 21:18:00 +0530

    added friend

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,8 +1,10 @@
 import { combineReducers, createStore } from "redux";
 import { LoginReducer } from "./Reucers";
+import friendsReducer from "./friendReducer";
 
 const rootReducers = combineReducers({
   auth: LoginReducer,
+  friendspage : friendsReducer,
 });
 
 export const store = createStore(rootReducers);
